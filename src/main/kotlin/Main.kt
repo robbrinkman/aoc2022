faf41e3867036fd981a9fa40740c6d7b2fc801a2
Rob Brinkman <rob.brinkman@gmail.com>
2022-12-01 18:54:42 +0100

    Fixed day one

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,12 +6,33 @@ fun main(args: Array<String>) {
 
 class AOC {
 
-    fun exercise01() {
-        readInputFile("/01/calories_example.txt").forEachLine { line ->
-            println(line)
+    data class Elf(val calories: Int)
 
+    fun exercise01() {
+
+
+        val elfs : MutableList<Elf> = mutableListOf()
+        var calorieCount =0;
+
+        // Turn into nice code
+        readInputFile("/01/input.txt").forEachLine { line ->
+            if (line.trim() == "") {
+                elfs.add(Elf(calorieCount))
+                calorieCount = 0;
+            } else {
+                calorieCount+= line.toInt()
+            }
         }
+
+        println("Most calories: ${findElfWithMostCalories(elfs)?.calories}")
+        println("Top three on calories: ${findTopThreeElves(elfs)}")
+        println("Top three calories total: ${findTopThreeElves(elfs).sumOf { it.calories }}")
     }
+
+    private fun findElfWithMostCalories(elfs : List<Elf>) = elfs.maxByOrNull { it.calories }
+
+    private fun findTopThreeElves(elfs : List<Elf>) = elfs.sortedBy { it.calories }.reversed().subList(0,3)
+
 
     private fun readInputFile(inputFile: String) : File {
         return File(javaClass::class.java.getResource(inputFile).path)
